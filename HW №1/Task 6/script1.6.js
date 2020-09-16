572b038ab8714947826b3c0af6d3d981351b1471
Oleg-Baltarovych <69357715+Oleg-Baltarovych@users.noreply.github.com>
2020-09-16 11:51:41 +0300

    Add files via upload

---
+++ HW №1/Task 6/script1.6.js
@@ -0,0 +1,22 @@
+// Ініціалізація змінних.
+let name = "Oleg";
+let lastName = "Baltarovych";
+let groupNumber = 533;
+let yearOfBirth = 1999;
+let maritalStatus = false;
+let anyInfo = null;
+let more;
+// Визначення типу змінних.
+console.log(typeof name);
+console.log(typeof lastName);
+console.log(typeof groupNumber);
+console.log(typeof yearOfBirth);
+console.log(typeof maritalStatus);
+console.log(typeof anyInfo);
+console.log(typeof more);
+// Виведення по порядку.
+console.log(groupNumber);
+console.log(yearOfBirth);
+console.log(maritalStatus);
+console.log(name);
+console.log(lastName);
