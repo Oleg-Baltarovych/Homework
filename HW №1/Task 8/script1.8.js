537806de2eae008fca3455ca2879618afb405fb3
Oleg-Baltarovych <69357715+Oleg-Baltarovych@users.noreply.github.com>
2020-09-17 20:22:27 +0300

    Delete script1.8.js

--- HW №1/Task 8/script1.8.js
+++
@@ -1,8 +0,0 @@
-let second = 1;
-let minute = second * 60;
-let secondInHour = minute * 60;
-let secondInDay = secondInHour * 24;
-let secondInMonth = secondInDay * 31;
-console.log(secondInHour);
-console.log(secondInDay);
-console.log(secondInMonth);
