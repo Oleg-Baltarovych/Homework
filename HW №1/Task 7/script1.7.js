a94a71e31b72d8871be21e760aa67f41a5e8c9c6
Oleg-Baltarovych <69357715+Oleg-Baltarovych@users.noreply.github.com>
2020-09-17 20:22:20 +0300

    Delete script1.7.js

--- HW №1/Task 7/script1.7.js
+++
@@ -1,4 +0,0 @@
-let login = prompt("Write your login");
-let email = prompt("Write your email");
-let password = prompt("Write your password");
-alert("Dear " + login + " , your email is " + email + " , your password is " + password + ".");
