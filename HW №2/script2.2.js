0ef352179406ecc4ee4753d40c102bae6753b2d3
Oleg-Baltarovych <69357715+Oleg-Baltarovych@users.noreply.github.com>
2020-09-30 15:37:53 +0300

    Update script2.2.js

--- HW №2/script2.2.js
+++ HW №2/script2.2.js
@@ -1,7 +1,9 @@
 let number = prompt("Введіть своє число.");
+
 if (number % 2 == 0 && number > 0) {
 	console.log("Ви ввели парне додатнє число.");
 };
+
 if (number % 7 == 0) {
 	console.log("Число є кратне числу 7.");
 };
