233c7cb0eff3b368cc0234673f1c7f90e025d7e2
Oleg-Baltarovych <69357715+Oleg-Baltarovych@users.noreply.github.com>
2020-10-02 14:03:53 +0300

    Update script4.5.js

--- HW №4/script4.5.js
+++ HW №4/script4.5.js
@@ -1,21 +1,21 @@
 function showUser(id) {
-	let result = {id: id};
-	if (id < 0) {
-		throw new Error("ID must not be negative: " + id);
-	}
-	return result;
+  let result = { id: id };
+  if (id < 0) {
+    throw new Error("ID must not be negative: " + id);
+  }
+  return result;
 }
 function showUsers(ids) {
-	let result = [];
-	for (let i = 0; i < ids.length; i++) {
-	try {
-		result[i] = showUser(ids[i]);
-	} catch (error) {
-		console.log(error.name);
-		console.log(error.message);
-	}
-	}
-	return result;
+  let result = [];
+  for (let i = 0; i < ids.length; i++) {
+    try {
+      result[i] = showUser(ids[i]);
+    } catch (error) {
+      console.log(error.name);
+      console.log(error.message);
+    }
+  }
+  return result;
 }
 
-showUsers([7, -12, 44, 22])
+showUsers([7, -12, 44, 22]);
