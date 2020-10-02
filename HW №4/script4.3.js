22eaaf8ca0748d84880040899389791f71ca9456
Oleg-Baltarovych <69357715+Oleg-Baltarovych@users.noreply.github.com>
2020-10-02 14:02:59 +0300

    Update script4.3.js

--- HW №4/script4.3.js
+++ HW №4/script4.3.js
@@ -1,20 +1,20 @@
 function checkAge() {
-	let age = +(prompt("Введіть свій вік."));
-	if (age <= 14 && age > 0) {
-		throw new Error("Ви ще надто молодий");
-	} else if (age === "" || age === " ") {
-		throw new Error("Поле пусте. Будь ласка вкажіть ваш вік.");
-	} else if (isNaN(age)) {
-		throw new Error("Введіть будь ласка числове значення.");
-	} else {
-		throw new Error("Ви ввели некоректне значення.")
-	}
-	return true;
+  let age = +prompt("Введіть свій вік.");
+  if (age <= 14 && age > 0) {
+    throw new Error("Ви ще надто молодий");
+  } else if (age === "" || age === " ") {
+    throw new Error("Поле пусте. Будь ласка вкажіть ваш вік.");
+  } else if (isNaN(age)) {
+    throw new Error("Введіть будь ласка числове значення.");
+  } else {
+    throw new Error("Ви ввели некоректне значення.");
+  }
+  return true;
 }
 try {
-	let result = checkAge();
-	alert(result);
+  let result = checkAge();
+  alert(result);
 } catch (error) {
-	alert(error.name);
-	alert(error.message);
+  alert(error.name);
+  alert(error.message);
 }
