4e08d6b54d1b04e15a5e23082d8789bdd5755ec6
Oleg-Baltarovych <69357715+Oleg-Baltarovych@users.noreply.github.com>
2020-10-02 14:00:47 +0300

    Update script3.5.js

--- HW №3/script3.5.js
+++ HW №3/script3.5.js
@@ -1,10 +1,10 @@
 function findMin() {
-	let min = arguments[0];
-	for (let i = 0; i < arguments.length; i++) {
-		if (min > arguments[i]) {
-			min = arguments[i];
-		}
-	}
-	return min;
+  let min = arguments[0];
+  for (let i = 0; i < arguments.length; i++) {
+    if (min > arguments[i]) {
+      min = arguments[i];
+    }
+  }
+  return min;
 }
-findMin(3,5,-99,20,-1,30,17,2,68,-4);
+findMin(3, 5, -99, 20, -1, 30, 17, 2, 68, -4);
