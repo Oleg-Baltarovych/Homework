9b8dc2e17cdf049c60f64a0c36d9acd8095930de
Oleg-Baltarovych <69357715+Oleg-Baltarovych@users.noreply.github.com>
2020-10-02 14:00:24 +0300

    Update script3.4.js

--- HW №3/script3.4.js
+++ HW №3/script3.4.js
@@ -2,7 +2,7 @@ let a = Math.round(prompt("Введіть ціле число."));
 let b = Math.round(prompt("Введіть значення степеня."));
 let result;
 function raiseToDegree(a, b) {
-	result = a ** b;
-	return result;
+  result = a ** b;
+  return result;
 }
-alert("Результат піднесення до степеня: " + raiseToDegree(a , b));
+alert("Результат піднесення до степеня: " + raiseToDegree(a, b));
