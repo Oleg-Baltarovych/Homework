835694d00632d176282f5a19b6e634d09bb8f12e
Oleg-Baltarovych <69357715+Oleg-Baltarovych@users.noreply.github.com>
2020-10-02 14:01:55 +0300

    Update script3.8.js

--- HW №3/script3.8.js
+++ HW №3/script3.8.js
@@ -1,13 +1,13 @@
 let strIn = "i love java script";
 function firstLatter(string) {
-let strOut = "";
-	for (i = 0; i < string.length; i++) {
-		if (string[i - 1] === " " || i === 0) {
-		strOut += string[i].toUpperCase();
-		} else {
-		strOut += string[i];
-		}
-	}
-return strOut;
+  let strOut = "";
+  for (i = 0; i < string.length; i++) {
+    if (string[i - 1] === " " || i === 0) {
+      strOut += string[i].toUpperCase();
+    } else {
+      strOut += string[i];
+    }
+  }
+  return strOut;
 }
 console.log(firstLatter(strIn));
