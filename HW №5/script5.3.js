fc4b8f6e99fbc670afdcc30387dc2372283eb8fd
Oleg-Baltarovych <69357715+Oleg-Baltarovych@users.noreply.github.com>
2020-10-02 14:04:54 +0300

    Update script5.3.js

--- HW №5/script5.3.js
+++ HW №5/script5.3.js
@@ -1,27 +1,27 @@
 class Person {
-	constructor(name, surname) {
-		this.name = name;
-		this.surname = surname;
-	}
-	showFullName() {
-		console.log(this.name, this.surname);
-	}
+  constructor(name, surname) {
+    this.name = name;
+    this.surname = surname;
+  }
+  showFullName() {
+    console.log(this.name, this.surname);
+  }
 }
 class Student extends Person {
-	constructor(name, surname, year) {
-		super(name, surname);
-		this.year = year;
-	}
-	showFullName(midleName) {
-		super.showFullName();
-		return this.midleName = midleName;
-	}
-	showCourse() {
-		let date = new Date();
-		let year = date.getFullYear();
-		let result = year - this.year;
-		return result;
-	}
+  constructor(name, surname, year) {
+    super(name, surname);
+    this.year = year;
+  }
+  showFullName(midleName) {
+    super.showFullName();
+    return (this.midleName = midleName);
+  }
+  showCourse() {
+    let date = new Date();
+    let year = date.getFullYear();
+    let result = year - this.year;
+    return result;
+  }
 }
 let stud1 = new Student("Petro", "Petrenko", 2015);
 console.log(stud1.showFullName("Petrovych"));
