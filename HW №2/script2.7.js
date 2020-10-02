a7842a6888f98ad5e03c52af821606b4c5830bfe
Oleg-Baltarovych <69357715+Oleg-Baltarovych@users.noreply.github.com>
2020-10-02 13:58:31 +0300

    Update script2.7.js

--- HW №2/script2.7.js
+++ HW №2/script2.7.js
@@ -2,25 +2,25 @@ let currentDate = new Date();
 let currentHours = currentDate.getHours();
 // Використання умовного оператора if/else if/else.
 if (currentHours > 5 && currentHours <= 11) {
-	console.log("Доброго ранку");
+  console.log("Доброго ранку");
 } else if (currentHours > 11 && currentHours <= 17) {
-	console.log("Доброго дня");
+  console.log("Доброго дня");
 } else if (currentHours > 17 && currentHours <= 23) {
-	console.log("Доброго вечора");
+  console.log("Доброго вечора");
 } else {
-	console.log("Доброї ночі");
+  console.log("Доброї ночі");
 }
 // Використання умовного оператора switch.
 switch (true) {
-	case currentHours > 5 && currentHours <= 11:
-	console.log("Доброго ранку");
-	break;
-	case currentHours > 11 && currentHours <= 17:
-	console.log("Доброго дня");
-	break;
-	case currentHours > 17 && currentHours <= 23:
-	console.log("Доброго вечора");
-	break;
-	default:
-	console.log("Доброї ночі");
+  case currentHours > 5 && currentHours <= 11:
+    console.log("Доброго ранку");
+    break;
+  case currentHours > 11 && currentHours <= 17:
+    console.log("Доброго дня");
+    break;
+  case currentHours > 17 && currentHours <= 23:
+    console.log("Доброго вечора");
+    break;
+  default:
+    console.log("Доброї ночі");
 }
